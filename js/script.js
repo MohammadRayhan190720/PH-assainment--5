80222a37ee61c3fbd48bbf741ac4a4f8f2ba94c4
MohammadRayhan190720 <rafirayhan260@gmail.com>
2024-09-24 01:44:57 +0600

    input validation

--- js/script.js
+++ js/script.js
@@ -12,13 +12,27 @@ document.getElementById("noakhali-donate-btn")
     const donateAmountNoakhali = inputValueById("donate-amount-noakhali");
 
     const balanceNoakhali = inputTextById("balance-noakhali");
+    const accountBalance = inputTextById("account-balance");
+
+    // validation
+    if (donateAmountNoakhali <= 0 || isNaN(donateAmountNoakhali)) {
+      return alert("invalid data found");
+    }
+
+     if (accountBalance < donateAmountNoakhali) {
+       return alert("insufficient balance");
+     }
+   
 
     const updateBalanceNoakhali = balanceNoakhali + donateAmountNoakhali;
-
-    document.getElementById("balance-noakhali").innerText = updateBalanceNoakhali;
+    
+    document.getElementById("balance-noakhali").innerText =
+      updateBalanceNoakhali;
 
     // deducted from the total account balance
-    const accountBalance = inputTextById("account-balance");
+    
+    
+
     const updateAccountBalance = accountBalance - donateAmountNoakhali;
     document.getElementById("account-balance").innerText = updateAccountBalance;
 
@@ -35,32 +49,44 @@ document.getElementById("noakhali-donate-btn")
   });
 
 // donate btn for feni
-document.getElementById("feni-donate-btn")
+  document.getElementById("feni-donate-btn")
   .addEventListener("click", function () {
     const donateAmountFeni = inputValueById("donate-amount-feni");
 
     const balanceFeni = inputTextById("balance-feni");
+
+    const accountBalance = inputTextById("account-balance");
+
+    // validation
+    if (donateAmountFeni <= 0 || isNaN(donateAmountFeni)) {
+      return alert("invalid data found");
+    }
+    // validation
+    if (accountBalance < donateAmountFeni) {
+      return alert("Insufficient balance");
+    }
 
     const updateBalanceFeni = balanceFeni + donateAmountFeni;
 
     document.getElementById("balance-feni").innerText = updateBalanceFeni;
 
     // deducted from the total account balance
-    const accountBalance = inputTextById("account-balance");
+    
+
     const updateAccountBalance = accountBalance - donateAmountFeni;
     document.getElementById("account-balance").innerText = updateAccountBalance;
 
     // show in history
 
-        const historyContainer = document.getElementById("history-container");
-        const div = document.createElement("div");
-        div.classList.add("border-2", "p-10"); 
-        div.innerHTML = `
+    const historyContainer = document.getElementById("history-container");
+    const div = document.createElement("div");
+    div.classList.add("border-2", "p-10");
+    div.innerHTML = `
       <P class="text-xl font-bold">${donateAmountFeni}  Taka is Donate for Flood Relief in Feni,Bangladesh</P>
       <P class="mt-3">${new Date().toISOString()}</P>
       `;
 
-        historyContainer.appendChild(div);
+    historyContainer.appendChild(div);
   });
 
   // donate btn for quote movement
@@ -70,25 +96,39 @@ document.getElementById("movement-donate-btn")
     const donateAmountMovement = inputValueById("donate-amount-movement");
 
     const balanceMovement = inputTextById("balance-movement");
+    
+    const accountBalance = inputTextById("account-balance");
+
+    // validation
+    if (donateAmountMovement <= 0 || isNaN(donateAmountMovement)) {
+      return alert("invalid data found");
+    }
+
+    // validation
+    if (accountBalance < donateAmountMovement) {
+      return alert("Insufficient balance");
+    }
 
     const updateBalanceMovement = balanceMovement + donateAmountMovement;
 
-    document.getElementById("balance-movement").innerText = updateBalanceMovement;
+    document.getElementById("balance-movement").innerText =
+      updateBalanceMovement;
 
     // deducted from the total account balance
-    const accountBalance = inputTextById("account-balance");
+    
+
     const updateAccountBalance = accountBalance - donateAmountMovement;
     document.getElementById("account-balance").innerText = updateAccountBalance;
 
     // show in history
 
-        const historyContainer = document.getElementById("history-container");
-        const div = document.createElement("div");
-        div.classList.add("border-2", "p-10");
-        div.innerHTML = `
+    const historyContainer = document.getElementById("history-container");
+    const div = document.createElement("div");
+    div.classList.add("border-2", "p-10");
+    div.innerHTML = `
        <P class="text-xl font-bold">${donateAmountMovement}  Taka is Donate for Injured in the Quota Movement</P>
       <P class="mt-3">${new Date().toISOString()}</P>
     `;
 
-        historyContainer.appendChild(div);
+    historyContainer.appendChild(div);
   });
